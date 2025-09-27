39cda198e8728e267957d5e51b097f5bc7a20f86
leila-digh <leiladabakuyo@cmail.carleton.ca>
2025-09-27 17:02:49 -0400

    commit

---
+++ MyWorkspace/Typing Game/script.js
@@ -0,0 +1,37 @@
+const quotes = {
+
+};
+
+let words = []
+let wordIndex = 0;
+
+let startTime = Date.now();
+
+//page elements
+const quoteElement = document.getElementById('quote');
+const messageElement = document.getElementById('message');
+const userInputElement = document.getElementById('user-in');
+
+document.getElementById('start').addEventListener('click', () => {
+    // get a quote at random, generate a random index, const
+    //const quotes = quotes[randomIndex]
+
+    // put quote in an array of words
+    // reset the word index 
+
+    // span class to add words
+    // vconver to a string to add to the inner html
+
+    //clear previous messages
+    //clear the textbox
+
+    //hilight the first word
+    //set focus (set the cursor on this element now)
+
+    // start the timer
+})
+
+userInputElement.addEventListener('input', (e) => {
+    //currentword
+    //current uder input
+})
